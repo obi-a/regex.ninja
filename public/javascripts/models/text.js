0d2ace8ca9dd0535b2b4912db6137cd1a7d5605f
Obi Akubue <obioraakubue@yahoo.com>
2015-07-13 00:11:20 -0400

    added rounded corners around regex matches

--- public/javascripts/models/text.js
+++ public/javascripts/models/text.js
@@ -6,6 +6,8 @@ var Text = Backbone.Model.extend({
   },
   searchText: function (){
     try {
+
+      if( _.isEmpty(this.get('regex')) ) { return this.get('plainText') };
       var re = new RegExp("(" + this.get('regex') + ")", "g");
       var plainText = this.get('plainText');
       this.set( "searchResult", plainText.replace(re, "<span class=\"highlight\">$1</span>") );
